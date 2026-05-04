8a7b60cd96d7d2a8bdea107f948bb9d72e0235f5
Dauboau <86164187+Dauboau@users.noreply.github.com>
2026-05-04 20:27:15 -0300

    Início!

---
+++ lalg_compiler/src/main/java/g5/Compiler.java
@@ -0,0 +1,13 @@
+package g5;
+
+/**
+ * Hello world!
+ *
+ */
+public class Compiler 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
